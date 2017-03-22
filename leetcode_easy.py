5be9d68ed13c0eb3485e365f9a35c1ae35482b95
Paulina Ng <qwnpng@gmail.com>
2017-03-22 12:17:27 -0700

    Finished first pass on hamming distance problem

---
+++ leetcode_easy.py
@@ -0,0 +1,37 @@
+# Hamming distance between two integers
+def hamming_dist(a, b):
+    """ # positions at which corresponding bits are different """
+
+    # convert both ints to binary
+    small_bin = int2bin(min(a, b))
+    big_bin = int2bin(max(a, b))
+
+    # compare each corresponding digit, incrementing a counter
+    big_len = len(big_bin)
+    dist = 0
+    for i in range(big_len - 1, -1, -1):
+        if i >= len(small_bin):
+            if big_bin[i] != 0:
+                dist += 1
+        elif big_bin[i] != small_bin[i]:
+            dist += 1
+
+    return dist
+
+
+def int2bin(num):
+    """ Convert integer to binary string"""
+
+    if num == 0:
+        return '0'
+
+    bin_str = ''
+    while num:
+        if num & 1 == 1:
+            bin_str = '1' + bin_str
+        else:
+            bin_str = '0' + bin_str
+
+        num /= 2
+
+    return bin_str
